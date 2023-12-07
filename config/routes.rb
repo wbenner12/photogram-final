05db2daa75453bc1d941e6f5082ca92bfc500987
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-12-07 23:34:38 +0000

    comments generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Comment resource:
+
+  # CREATE
+  post("/insert_comment", { :controller => "comments", :action => "create" })
+          
+  # READ
+  get("/comments", { :controller => "comments", :action => "index" })
+  
+  get("/comments/:path_id", { :controller => "comments", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_comment/:path_id", { :controller => "comments", :action => "update" })
+  
+  # DELETE
+  get("/delete_comment/:path_id", { :controller => "comments", :action => "destroy" })
+
+  #------------------------------
+
   root "photos#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
