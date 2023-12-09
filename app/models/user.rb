33e9d21d93277b6998c1df14ffbb93b9bf64bc21
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-12-09 00:25:44 +0000

    user model should be good

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,23 +21,28 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates(:username, presence: true)
   validates(:username, uniqueness: true)
   
-  has_many  :likes, class_name: "Like", foreign_key: "fan_id", dependent: :destroy
-  has_many  :comments, class_name: "Comment", foreign_key: "author_id", dependent: :destroy
-  has_many  :own_photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy
-  has_many  :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy
-  has_many  :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id", dependent: :destroy
+  has_many(:likes, class_name: "Like", foreign_key: "fan_id", dependent: :destroy)
+  has_many(:comments, class_name: "Comment", foreign_key: "author_id", dependent: :destroy)
+  has_many(:own_photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy)
+  has_many(:sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy)
+  has_many(:received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id", dependent: :destroy)
        
-  has_many :following, through: :sent_follow_requests, source: :recipient
-  has_many :followers, through: :received_follow_requests, source: :sender
-  has_many :liked_photos, through: :likes, source: :photo
-  has_many :feed, through: :following, source: :own_photos
-  has_many :activity, through: :following, source: :liked_photos
+  has_many(:following, through: :sent_follow_requests, source: :recipient)
+  has_many(:followers, through: :received_follow_requests, source: :sender)
+  has_many(:liked_photos, through: :likes, source: :photo)
+  has_many(:commented_photos, through: :comments, source: :photo)
+  has_many(:feed, through: :following, source: :own_photos)
+  has_many(:activity, through: :following, source: :liked_photos)
+  has_many(:discover, through: :leaders, source: :liked_photos)
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_many(:accepted_sent_follow_requests, -> { where status: "accepted" }, class_name: "FollowRequest", foreign_key: :sender_id)
+  
+  has_many(:accepted_received_follow_requests, -> { where status: "accepted" }, class_name: "FollowRequest", foreign_key: :recipient_id)
+  
+
 end
