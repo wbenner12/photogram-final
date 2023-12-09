3fe8de881556a46820f2ab9ab8be51f63075d824
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-12-09 01:20:08 +0000

    follow requests controller good

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -20,14 +20,14 @@ class FollowRequestsController < ApplicationController
   def create
     the_follow_request = FollowRequest.new
     the_follow_request.recipient_id = params.fetch("query_recipient_id")
-    the_follow_request.sender_id = params.fetch("query_sender_id")
+    the_follow_request.sender_id = current_user.id
     the_follow_request.status = params.fetch("query_status")
 
     if the_follow_request.valid?
       the_follow_request.save
-      redirect_to("/follow_requests", { :notice => "Follow request created successfully." })
+      redirect_to("/users", { :notice => "Follow request created successfully." })
     else
-      redirect_to("/follow_requests", { :alert => the_follow_request.errors.full_messages.to_sentence })
+      redirect_to("/users", { :alert => the_follow_request.errors.full_messages.to_sentence })
     end
   end
 
@@ -38,6 +38,7 @@ class FollowRequestsController < ApplicationController
     the_follow_request.recipient_id = params.fetch("query_recipient_id")
     the_follow_request.sender_id = params.fetch("query_sender_id")
     the_follow_request.status = params.fetch("query_status")
+    the_follow_request.password = params.fetch("query_password")
 
     if the_follow_request.valid?
       the_follow_request.save
