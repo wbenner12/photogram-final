a34cf71fa2e602c779439d81c5f975f071dc4531
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-12-09 01:15:37 +0000

    likes controller done

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,25 +8,31 @@ class LikesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    matching_photo = Photo.where({ :id => params.fetch("path_id") })
+    @the_photo = matching_photo.at(0)
 
-    matching_likes = Like.where({ :id => the_id })
+    matching_likes = Like.where({ :fan_id => current_user.id, :photo_id => @the_photo.id })
+    the_like = matching_likes.at(0)
 
-    @the_like = matching_likes.at(0)
+    if the_like.nil?
+      @liked = false
+    else
+      @liked = true
+    end
 
-    render({ :template => "likes/show" })
+    render({ :template => "photos/show" })
   end
 
   def create
     the_like = Like.new
-    the_like.fan_id = params.fetch("query_fan_id")
+    the_like.fan_id = current_user.id
     the_like.photo_id = params.fetch("query_photo_id")
 
     if the_like.valid?
       the_like.save
-      redirect_to("/likes", { :notice => "Like created successfully." })
+      redirect_to("/photos/" + the_like.photo_id.to_s, { :notice => "Like created successfully." })
     else
-      redirect_to("/likes", { :alert => the_like.errors.full_messages.to_sentence })
+      redirect_to("/photos/" + the_like.photo_id.to_s, { :alert => the_like.errors.full_messages.to_sentence })
     end
   end
 
@@ -46,11 +52,15 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path_id")
-    the_like = Like.where({ :id => the_id }).at(0)
-
-    the_like.destroy
-
-    redirect_to("/likes", { :notice => "Like deleted successfully."} )
+    
+    matching_likes = Like.where({ :fan_id => current_user.id, :photo_id => params.fetch("query_photo_id") })
+    @the_like = matching_likes.at(0)
+    
+    if @the_like.nil?
+      redirect_to("/photos/" + params.fetch("query_photo_id"), { :alert => "Like not found." })
+    else
+      @the_like.destroy
+      redirect_to("/photos/" + params.fetch("query_photo_id"), { :notice => "Like deleted successfully." })
+    end
   end
 end
