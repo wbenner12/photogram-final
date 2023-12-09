bd648799d9d4ae4a3a07bbc8afba73f791932398
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-12-09 00:46:25 +0000

    All models should be done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,6 +12,6 @@
 class Comment < ApplicationRecord
   validates(:author, { :presence => true })
 
-  belongs_to :author, required: true, class_name: "User", foreign_key: "author_id"
-  belongs_to :photo, required: true, class_name: "Photo", foreign_key: "photo_id"
+  belongs_to(:author, required: true, class_name: "User", foreign_key: "author_id")
+  belongs_to(:photo, required: true, class_name: "Photo", foreign_key: "photo_id")
 end
