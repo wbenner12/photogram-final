6dc7a43e53ca04870aee7b8ed34a4bac75822b09
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-12-09 00:38:42 +0000

    photo.rb fixed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,13 +12,13 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
-  validates(:owner, { :presence => true })
+  validates(:poster, { :presence => true })
   
-  belongs_to :owner, required: true, class_name: "User", foreign_key: "owner_id"
-  has_many  :likes, class_name: "Like", foreign_key: "photo_id", dependent: :destroy
-  has_many  :comments, class_name: "Comment", foreign_key: "photo_id", dependent: :destroy
+  belongs_to(:owner, required: true, class_name: "User", foreign_key: "owner_id")
+  has_many(:likes, class_name: "Like", foreign_key: "photo_id", dependent: :destroy)
+  has_many(:comments, class_name: "Comment", foreign_key: "photo_id", dependent: :destroy)
 
-  has_many :fans, through: :likes, source: :fan
-  has_many :followers, through: :owner, source: :following
-  has_many :fan_followers, through: :fans, source: :following
+  has_many(:fans, through: :likes, source: :fan)
+  has_many(:followers, through: :owner, source: :following)
+  has_many(:fan_followers, through: :fans, source: :following)
 end
